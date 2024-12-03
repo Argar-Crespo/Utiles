2e8908c631fb4d34b96ea69a87a0b9c4b3c87639
Árgar Crespo <argar.crespo.7e8@itb.cat>
2024-12-03 10:06:44 +0100

    QUE TE SUBAS YA COÑOOO

--- Utilidades_LOK/Utilities.kt
+++ Utilidades_LOK/Utilities.kt
@@ -1,5 +1,4 @@
 package Utilidades_LOK
-
 import java.util.*
 
 val scan: Scanner = Scanner(System.`in`)
@@ -43,7 +42,7 @@ fun readWord(pMessageIn: String
  * @return outputValue Output value
  */
 fun readSentence(pMessageIn: String
-             , pMessageErrorDT: String
+                 , pMessageErrorDT: String
 ): String{
 
     var outputValue: String = ""
@@ -269,10 +268,10 @@ fun readFloat(pMessageIn: String
  * @return outputValue Output value
  */
 fun readFloat(pMessageIn: String
-            , pMessageErrorDT: String
-            , pMessageErrorDV: String
-            , pMin: Float
-            , pMax: Float
+              , pMessageErrorDT: String
+              , pMessageErrorDV: String
+              , pMin: Float
+              , pMax: Float
 ): Float{
 
     var outputValue: Float = 0.0f
@@ -296,4 +295,199 @@ fun readFloat(pMessageIn: String
     }while(!correctDataType)
 
     return outputValue
+}
+
+/**
+ * Document kt dedicat al emmagatzematge de funcions de lectura de Int, Double, Float, Long, Word, Char per poder usar
+ * en les funcions dels projectes.
+ * @author Raimon Izard, David Marin i Martí Vilàs.
+ */
+fun llegirInt(pMessageIn: String
+              , pMessageErrorDT: String
+): Int{
+    val scanner=Scanner(System.`in`)
+    var num:Int=0
+    var error = true
+    do {if (scanner.hasNextInt()){
+        num=scanner.nextInt()
+        error=false
+    }else {
+        scanner.nextLine()
+        println("${RED}ERROR HA DE SER UN NÚMERO ENTER${RESET}")
+    }
+    }while (error)
+    return num
+}
+
+fun llegirDouble(pMessageIn: String
+                 , pMessageErrorDT: String
+):Double {
+    val scanner=Scanner(System.`in`)
+    var num:Double=0.0
+    var error = true
+    do {if (scanner.hasNextDouble()){
+        num=scanner.nextDouble()
+        error=false
+    }else {
+        scanner.nextLine()
+    }
+    }while (error)
+    return num
+}
+
+fun llegirFloat( pMissatgeEntrada: String, pMissatgeError: String):Float {
+    val scanner=Scanner(System.`in`)
+    var num:Float = 0.0f
+    var error = true
+    do {if (scanner.hasNextFloat()){
+        num=scanner.nextFloat()
+        error=false
+    }else {
+        scanner.nextLine()
+    }
+    }while (error)
+    return num
+}
+
+fun llegirLong():Long {
+    val scanner=Scanner(System.`in`)
+    var num:Long=0
+    var error = true
+    do {if (scanner.hasNextLong()){
+        num=scanner.nextLong()
+        error=false
+    }else {
+        scanner.nextLine()
+    }
+    }while (error)
+    return num
+}
+
+fun llegirWord(pMessageIn: String, pMessageErrorDT: String): String {
+    val scanner=Scanner(System.`in`)
+    val outputValue: String
+    do {
+        println(pMessageIn)
+        if (!scanner.hasNext()) {
+            println("ERROR: $pMessageErrorDT")
+            scanner.next()
+        } else {
+            outputValue = scanner.next()
+            scanner.nextLine()
+            return outputValue
+        }
+    } while (true)
+}
+
+fun llegirChar( pCase: Int = -1): Char {
+    val scanner=Scanner(System.`in`)
+    val outputValue: Char
+    do {
+        if (!scanner.hasNext()) {
+            scanner.next()
+        } else {
+            outputValue = scanner.next()[0]
+            scanner.nextLine() // clear buffer
+            when (pCase) {
+                0 -> return outputValue.lowercaseChar()
+                1 -> return outputValue.uppercaseChar()
+                else -> return outputValue
+            }
+        }
+    } while (true)
+}
+
+fun llegirStrings(pMessageIn: String, pMessageErrorDT: String): String{
+    val scanner=Scanner(System.`in`)
+
+    var outputValue: String = ""
+    var correctDataType: Boolean = false
+
+    do{
+        println(pMessageIn)
+        correctDataType = scanner.hasNext()
+
+        if (!correctDataType){
+            println(RED_BACKGROUND_BRIGHT + "ERROR: " + pMessageErrorDT + RESET)
+        }else{
+            outputValue = scanner.nextLine()
+        }
+    }while(!correctDataType)
+
+    return outputValue
+}
+
+/**
+ * Aquest document està destinat a emmagatzemar funcions generals que puguin ser utilitzades en diferents projectes.
+ * @author Martí Vilàs
+ */
+fun llegirSioNo() : Boolean {
+    var scanner=Scanner(System.`in`)
+    val resposta=scanner.nextLine().lowercase()
+
+    return when (resposta) {
+        "s" -> true
+        "n" -> false
+        else -> {
+            println("Resposta no vàlida la resposta ha de ser S o N")
+            llegirSioNo()
+        }
+    }
+}
+
+fun readInt ():Int {
+    val scanner=Scanner(System.`in`)
+    var num:Int= 0
+    var error = true
+    do {if (scanner.nextInt()==1||scanner.nextInt()==2){
+        num=scanner.nextInt()
+        error=false
+    }else {
+        scanner.nextLine()
+    }
+    }while (error)
+
+    return num
+
+}
+fun llegirEntre1o2(): Int {
+    val scanner = Scanner(System.`in`)
+    var numero: Int = 0
+    var error = true
+
+    do {
+        if (scanner.hasNextInt()) {
+            numero = scanner.nextInt()
+            if (numero in 1..2) {
+                error = false
+            } else {
+                println("Per favor, ingressa un valor vàlid (1 o 2).")
+            }
+        } else {
+            println("Per favor, ingressa un número enter.")
+            scanner.nextLine()
+        }
+    } while (error)
+    return numero
+}
+
+fun llegirTotsInt(): Int {
+    val scanner = Scanner(System.`in`)
+    var numero: Int = 0
+    var error = true
+
+    do {
+        if (scanner.hasNextInt()) {
+            numero = scanner.nextInt()
+            if (numero > 0) {
+                error = false
+            } else {
+                println("Per favor, ingressa un valor vàlid enter mayor que 0.")
+            }
+        } else {
+            println("Per favor, ingressa un número enter.")
+            scanner.nextLine()
+        }
+    } while (error)
+    return numero
 }
